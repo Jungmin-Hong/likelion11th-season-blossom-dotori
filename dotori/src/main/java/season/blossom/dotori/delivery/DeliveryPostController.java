fe06ce6d5550871f2c95b238633b4d50902871ae
Jungmin-Hong <jungmin011107@gmail.com>
2023-02-18 17:25:19 +0900

    feat: Add delivery post api base 2

--- dotori/src/main/java/season/blossom/dotori/delivery/DeliveryPostController.java
+++ dotori/src/main/java/season/blossom/dotori/delivery/DeliveryPostController.java
@@ -2,28 +2,37 @@ package season.blossom.dotori.delivery;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class DeliveryPostController {
     private DeliveryPostService deliveryPostService;
 
+    // 목록
     @GetMapping("/")
-    public String list() {
+    public String list(Model model) {
+        List<DeliveryPostDto> deliveryPostList = deliveryPostService.getDeliveryPostList();
+        model.addAttribute("deliveryPostList", deliveryPostList);
         return "deliveryPost/list";
     }
 
+    // 글 작성
     @GetMapping("/post")
     public String write() {
         return "deliveryPost/write";
     }
 
+    // 글 작성 완료 후
     @PostMapping("/post")
     public String write(DeliveryPostDto deliveryPostDto) {
         deliveryPostService.savePost(deliveryPostDto);
-
         return "redirect:/";
     }
+
+    //
 }
--- dotori/src/main/java/season/blossom/dotori/delivery/DeliveryPostService.java
+++ dotori/src/main/java/season/blossom/dotori/delivery/DeliveryPostService.java
@@ -6,6 +6,9 @@ import org.springframework.transaction.annotation.Transactional;
 import season.blossom.dotori.delivery.DeliveryPostDto;
 import season.blossom.dotori.delivery.DeliveryPostRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @AllArgsConstructor
 @Service
 public class DeliveryPostService {
@@ -14,5 +17,25 @@ public class DeliveryPostService {
     @Transactional
     public Long savePost(DeliveryPostDto deliveryPostDto) {
         return deliveryPostRepository.save(deliveryPostDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<DeliveryPostDto> getDeliveryPostList() {
+        List<DeliveryPost> deliveryPosts = deliveryPostRepository.findAll();
+        List<DeliveryPostDto> deliveryPostList = new ArrayList<>();
+
+        for ( DeliveryPost deliveryPost : deliveryPosts) {
+            DeliveryPostDto deliveryPostDto = DeliveryPostDto.builder()
+                    .id(deliveryPost.getId())
+                    .title(deliveryPost.getTitle())
+                    .content(deliveryPost.getContent())
+                    .writer(deliveryPost.getWriter())
+                    .createdDate(deliveryPost.getCreatedDate())
+                    .build();
+
+            deliveryPostList.add(deliveryPostDto);
+        }
+
+        return deliveryPostList;
     }
 }
