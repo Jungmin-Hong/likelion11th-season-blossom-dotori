c71c03f5b1ea5f16262cb4e64057ad8da287c442
Jungmin-Hong <jungmin011107@gmail.com>
2023-02-23 19:07:46 +0900

    feat: Add mypage 2

--- dotori/src/main/java/season/blossom/dotori/delivery/DeliveryPostController.java
+++ dotori/src/main/java/season/blossom/dotori/delivery/DeliveryPostController.java
@@ -87,4 +87,10 @@ public class DeliveryPostController {
         return ResponseEntity.status(HttpStatus.OK).body(deliveryPosts);
     }
 
+//    @GetMapping("/api/mypage/comment/delivery")
+//    public ResponseEntity<List<DeliveryPostReturnDto>> getMyComment(@AuthenticationPrincipal CustomUserDetail customUserDetail) {
+//        User user = customUserDetail.getUser();
+//        List<DeliveryPostReturnDto> deliveryPosts = d
+//    }
+
 }
