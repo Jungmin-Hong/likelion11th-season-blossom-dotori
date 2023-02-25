0014d43b1eecd59b855f21adcf787be51dba4f46
guitarcode <choicco89@gmail.com>
2023-02-25 11:03:06 +0900

    fix: 작성 오류 수정

--- dotori/src/main/java/season/blossom/dotori/config/WebMvcConfig.java
+++ dotori/src/main/java/season/blossom/dotori/config/WebMvcConfig.java
@@ -9,10 +9,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+<<<<<<< HEAD
                 .allowedOrigins("http://localhost:3000", "http://localhost:5500", "http://localhost:52338")
                 .allowCredentials(true)
                 .allowedHeaders("*")
                 .exposedHeaders("Set-Cookie")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE", "HEAD");
+=======
+                .allowedOrigins("http://localhost:3000", "http://localhost:5500", "http://localhost:8080")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE", "HEAD")
+                .allowedHeaders("content-type")
+                .exposedHeaders("Set-Cookie")
+                .maxAge(1000)
+                .allowCredentials(true);
+>>>>>>> 434e70a (fix: 작성 오류 수정)
     }
 }
